fcf082160b6f358b6f4917f70824b472ef69c9e7
Fikri <fuadfikri01@gmail.com>
2019-02-17 11:37:26 +0700

    migration santri

---
+++ database/migrations/2019_02_17_033250_create_santris_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSantrisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('santris', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->date('tgl_lahir');
+            $table->integer('RT');
+            $table->integer('RW');
+            $table->integer('no_rumah')->nullable();
+            $table->string('url_foto')->nullable();
+            $table->string('sekolah');
+            $table->string('tahun_masuk_tpa');
+            $table->string('nama_orang_tua');
+            $table->string('jenis_kelamin');
+            $table->integer('kelas_id')->unsigned()->nullable();
+            $table->foreign('kelas_id')->references('id')->on('kelas')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('santris');
+    }
+}
