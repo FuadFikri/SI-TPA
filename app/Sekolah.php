f21c1c3e8b9298feb554dded3f03ef1c05090f83
Fikri <fuadfikri01@gmail.com>
2019-03-23 11:13:02 +0700

    guarded sekolah model

--- app/Sekolah.php
+++ app/Sekolah.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sekolah extends Model
 {
+    protected $guarded=['id'];
+
     public function santris()
     {
         return $this->hasMany('App\Santri');
