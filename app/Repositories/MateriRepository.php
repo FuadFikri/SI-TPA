75ef8b956708c3557d61511457521a8e6b12397f
Fikri <fuadfikri01@gmail.com>
2019-03-26 22:12:59 +0700

    materi repository dan service

---
+++ app/Repositories/MateriRepository.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Repositories;
+use App\Materi;
+use Illuminate\Database\Eloquent\Model;
+
+class MateriRepository extends CrudRepository{
+    public $model;
+
+    public function __construct(Materi $materi)
+    {
+        $this->model = $materi;
+    }
+
+    public function data($request)
+    {
+        $data = [
+            'judul' => $request->judul,
+            'deskripsi' => $request->deskripsi,
+            'parameter_kelulusan' => $request->parameter_kelulusan,
+            'link_materi' => $request->link_materi,
+            'kelas_id' => $request->kelas_id
+        ];
+        return $data;
+    }
+
+    public function materiUjianByKelas($kelas_id)
+    {
+        return $this->model::where('kelas_id',$kelas_id)->get();
+    }
+
+}
+
+?>
---
+++ app/Services/MateriService.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Services;
+
+use App\Materi;
+use App\Repositories\MateriRepository;
+
+class MateriService
+{
+    protected $materiRepository;
+
+    public function __construct(MateriRepository $materiRepository)
+    {
+        $this->materiRepository = $materiRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->materiRepository->index();
+    }
+
+    public function store($request)
+    {
+        return $this->materiRepository->store($request);
+    }
+    public function showDetail($id)
+    {
+        return $this->materiRepository->show($id);
+    }
+    public function edit($id)
+    {
+        return $this->materiRepository->show($id);
+    }
+    public function update($request, $id)
+    {
+        return $this->materiRepository->update($request, $id);
+        
+    }
+    public function delete($id)
+    {
+        return $this->materiRepository->delete($id);
+    }
+    public function count()
+    {
+        return $this->materiRepository->count();
+    }
+
+    public function getMateriUjianByKelas($kelas_id)
+    {
+        return $this->materiRepository->materiUjianByKelas($kelas_id);
+    }
+
+}
