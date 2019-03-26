46338ee6f7509b7db1a15077dd4baff5c236ee3b
Fikri <fuadfikri01@gmail.com>
2019-03-26 22:15:30 +0700

    getAllUjian getAllSantri

--- app/Http/Controllers/UjianController.php
+++ app/Http/Controllers/UjianController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 use App\Services\UjianService;
+use App\Services\SantriService;
+use App\Services\MateriService;
 use App\Ujian;
 use Illuminate\Http\Request;
 
 class UjianController extends Controller
 {
-    protected $ujianService;
-    public function __construct(UjianService $ujianService)
+    protected $ujianService, $santriService, $materiService;
+    public function __construct(UjianService $ujianService,
+                                SantriService $santriService,
+                                MateriService $materiService)
     {
         $this->ujianService = $ujianService;
+        $this->santriService = $santriService;
+        $this->materiService = $materiService;
     }
     public function index()
     {
@@ -55,4 +61,35 @@ class UjianController extends Controller
             return redirect()->route('ujian.index')->with('status','data berhasil dihapus');
         }
     }
+
+    //  user = ustadz/ah
+
+    // menampilkan daftar ujian
+    public function getUjian()
+    {
+        $daftar_ujian = $this->ujianService->getAll();
+        return view('ustadz.daftar-ujian',compact('daftar_ujian'));
+    }
+
+    // menampilkan daftar santri
+    public function getAllSantri(Request $request)
+    {
+        $ujian_id = $request->get('ujian_id');
+        // $ujian = $this->ujianService->showDetail($ujian_id);
+        $daftar_santri =  $this->santriService->getAll();
+        return view('ustadz.daftar-santri',compact('daftar_santri','ujian_id'));
+    }
+
+    public function getSelectedSantri(Request $request)
+    {
+        // $ujian = $this->ujianService->showDetail($request->ujian_id);
+        $santri = $this->santriService->showDetail($request->santri_id);
+        return  $this->materiService->getMateriUjianByKelas($santri->kelas_id);
+    }
+    
+    public function storeNilaiTes()
+    {
+
+    }
+
 }
