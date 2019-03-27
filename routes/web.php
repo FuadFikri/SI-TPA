a731ce82769691f705047b9575eb19d7235419d1
Fikri <fuadfikri01@gmail.com>
2019-03-27 09:00:36 +0700

    routing penilaian by ustadz

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('santri/import','SantriController@get_import');
-// Route::post('santri/import','SantriController@post_import');
+Route::prefix('ustadz')->group(function(){
+    Route::get('/','UjianController@getUjian');
+    Route::get('pilih-santri','UjianController@getAllSantri')->name('pilih-santri');
+    Route::post('penilaian','UjianController@getSelectedSantri');
+    // Route::post('penilaian','UjianController@storeNilaiTes');
+});
+
+
 Route::resource('santri','SantriController');
 Route::resource('ujian','UjianController');
 Route::resource('user','UserController');
