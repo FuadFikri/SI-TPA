3a4aea7f4143a4d0485765ffa3fcd20346297386
Fikri <fuadfikri01@gmail.com>
2019-03-29 09:36:13 +0700

    jquery, bootstrap, pooper, select2 offline

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -11,6 +11,8 @@
   <title>Larashop @yield("title")</title>
   <link rel="stylesheet" href="{{asset('polished/polished.min.css')}}">
   <link rel="stylesheet" href="{{asset('polished/iconic/css/open-iconic-bootstrap.min.css')}}">
+  <link href="{{asset('css/select2.min.css')}}" rel="stylesheet" />
+ 
 
   <style>
     .grid-highlight {
@@ -99,7 +101,7 @@
               <div class="col-md-12 pl-3 pt-2">
                   <div class="pl-3">
                       <h3>@yield("pageTitle")</h3>
-                      <br/  
+                      <br/>
                   </div>
               </div>
             </div>
@@ -110,11 +112,20 @@
       </div>
   </div>
 
-  <script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="{{asset('js\jquery-3.3.1.min.js')}}" crossorigin="anonymous"></script>
+<script src="{{asset('js\popper.min.js')}}" crossorigin="anonymous"></script>
+<script src="{{asset('js\bootstrap.min.js')}}" crossorigin="anonymous"></script>
+  
+{{-- <script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+crossorigin="anonymous"></script>
+<script
+src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+crossorigin="anonymous"></script> --}}
+@yield('footer-scripts')
 </body>
 </html>
