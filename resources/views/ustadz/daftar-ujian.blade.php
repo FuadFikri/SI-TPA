dec975f2d9885c74552484a5616484f574b98488
Fikri <fuadfikri01@gmail.com>
2019-03-30 09:06:30 +0700

    disabled ujian if ujian->status==0

--- resources/views/ustadz/daftar-ujian.blade.php
+++ resources/views/ustadz/daftar-ujian.blade.php
@@ -3,18 +3,24 @@
 @section('content')
 @foreach ($daftar_ujian as $ujian)
 <div class="col-md-6 col-lg-12 col-12 mb-2 col-sm-9">
-    <a href="{{route('pilih-santri',['ujian_id'=> $ujian->id])}}" >
-    <div class="media shadow-sm p-0 bg-{{$ujian->status==1? 'primary' : 'primary-lightest'}} rounded text-light">
-        <span class="oi top-0 rounded-left oi-task h-100 bg-{{$ujian->status==1? 'primary' : 'primary-lightest'}} text-light p-4 fs-5"></span>
-        <div class="media-body p-2">
-        <h6 class="media-title m-0">Ujian TPA AL-Kariim Pogung Lor tahun {{$ujian->tahun}}</h6>
-            <div class="media-text">
-            <h3>Semester {{$ujian->semester}}</h3>
-                <h5>{{$ujian->keterangan}}</h5>
+    @if ($ujian->status==0)
+        <a disabled>
+    @else
+        <a href="{{route('pilih-santri',['ujian_id'=> $ujian->id])}}">
+    @endif
+            <div
+                class="media shadow-sm p-0 bg-{{$ujian->status==1? 'primary' : 'primary-lightest'}} rounded text-light">
+                <span
+                    class="oi top-0 rounded-left oi-task h-100 bg-{{$ujian->status==1? 'primary' : 'primary-lightest'}} text-light p-4 fs-5"></span>
+                <div class="media-body p-2">
+                    <h6 class="media-title m-0">Ujian TPA AL-Kariim Pogung Lor tahun {{$ujian->tahun}}</h6>
+                    <div class="media-text">
+                        <h3>Semester {{$ujian->semester}}</h3>
+                        <h5>{{$ujian->keterangan}}</h5>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</a>
+        </a>
 </div>
 @endforeach
 
