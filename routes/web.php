4b016160acb8a17893ce328bfe3f790ba1b08aaa
Fikri <fuadfikri01@gmail.com>
2019-04-14 12:15:25 +0700

    route auth

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,41 @@
 <?php
+
+Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
 
 // navigation
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/', 'DashboardController@index')->name('dashboard');
 
-Route::get('santri/import','SantriController@get_import');
-Route::get('materi/searchMateriAjax','MateriController@searchMateriAjax');
-Route::get('lihat-nilai','UjianController@hasilUjianPerMateri');
-Route::get('ujian/cetak-raport','UjianController@cetak_raport');
-Route::prefix('ustadz')->group(function(){
-    Route::get('/','UjianController@getUjian')->name('ustadz');
-    Route::get('pilih-santri','UjianController@getAllSantri')->name('pilih-santri');
-    Route::post('penilaian','UjianController@getSelectedSantri');
-    Route::post('simpan-nilai','UjianController@storeNilaiTes');
+Route::group(['middleware' => ['auth']], function () {
+    
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    
+    Route::get('santri/import','ExcelController@get_import');
+    Route::post('santri/import','ExcelController@post_import');
+    Route::get('materi/searchMateriAjax','MateriController@searchMateriAjax');
+    Route::get('lihat-nilai','PenilaianController@hasilUjianPerMateri');
+    Route::get('ujian/cetak-raport','PenilaianController@cetak_raport');
+    Route::prefix('ustadz')->group(function(){
+        Route::get('/','PenilaianController@getUjian')->name('ustadz');
+        Route::get('pilih-santri','PenilaianController@getAllSantri')->name('pilih-santri');
+        Route::post('penilaian','PenilaianController@getSelectedSantri');
+        Route::post('simpan-nilai','PenilaianController@storeNilaiTes');
+    });
+    
+    Route::resource('santri','SantriController');
+    
+    
+    Route::resource('ujian','UjianController');
+    Route::resource('user','UserController');
+    Route::resource('kelas','KelasController');
+    Route::resource('sekolah','SekolahController');
+    Route::resource('materi','MateriController');
+    
+    Route::get('data-master','DataMasterList');
+    Route::get('ruang-srawung',function(){
+        return view('UnderConstruction');
+    });
 });
 
-
-Route::resource('santri','SantriController');
-Route::resource('ujian','UjianController');
-Route::resource('user','UserController');
-Route::resource('kelas','KelasController');
-Route::resource('sekolah','SekolahController');
-Route::resource('materi','MateriController');
-
-Route::get('data-master','DataMasterList');
 
