f50f7c60adcb4e279a4c41a069436bf03d49f576
Fikri <fuadfikri01@gmail.com>
2019-04-14 11:06:04 +0700

    app name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.global')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -6,15 +6,16 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
+
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    <h5>Table with pagination</h5>
+
+                    You are logged in!
                 </div>
-                
             </div>
         </div>
     </div>
